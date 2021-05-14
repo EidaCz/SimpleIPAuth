3f746492cdedeb24ba17d8aaaa402f740b44c748
EidaCz <eida@eida.cz>
2021-05-14 18:21:18 +0200

    updated IPv4 matcher to support generic interface

--- src/main/java/cz/eida/minecraft/sipauth/ipmatcher/IPv4Matcher.java
+++ src/main/java/cz/eida/minecraft/sipauth/ipmatcher/IPv4Matcher.java
@@ -1,13 +1,15 @@
-package cz.eida.minecraft.sipauth;
+package cz.eida.minecraft.sipauth.ipmatcher;
 
 import java.util.List;
 
 /**
  * Simple IPv4 address/network matcher.
  */
-public class IPv4Matcher {
+public class IPv4Matcher implements IIPMatcher {
 
-    /** IPv4 address */
+    /**
+     * IPv4 address
+     */
     private long ip = 0x00000000L;
 
     /**
@@ -21,8 +23,27 @@ public class IPv4Matcher {
 
         int i;
         for (i = 0; i < 4; i++) {
-            this.ip += Integer.parseInt(splitIP[i]) << (24 - (8 * i));
+            this.ip += Long.parseLong(splitIP[i]) << (24 - (8 * i));
         }
+    }
+
+    /**
+     * Validates input as IPv4 address.
+     *
+     * @param address IPv4 with optional CIDR mask.
+     * @return given address is valid
+     */
+    public static boolean isValid(String address) {
+        return address.matches("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\/([0-9]|[1-2][0-9]|3[0-2]))?$");
+    }
+
+    /**
+     * IPv4 single host mask - 32 bits.
+     *
+     * @return "32"
+     */
+    public String getSingleHostMask() {
+        return "32";
     }
 
     /**
@@ -31,6 +52,7 @@ public class IPv4Matcher {
      * @param ipNet network in CIDR format A.B.C.D/M
      * @return IP belongs to this network
      */
+    @Override
     public boolean match(String ipNet) {
 
         try {
@@ -41,7 +63,7 @@ public class IPv4Matcher {
 
             int i;
             for (i = 0; i < 4; i++) {
-                base += Integer.parseInt(splitBase[i]) << (24 - (8 * i));
+                base += Long.parseLong(splitBase[i]) << (24 - (8 * i));
             }
 
             return ((base & mask) == (this.ip & mask));
@@ -57,6 +79,7 @@ public class IPv4Matcher {
      * @param ipNets array of networks in CIDR format A.B.C.D/M
      * @return IP belongs to some of given network
      */
+    @Override
     public boolean matchAny(String[] ipNets) {
         for (String cidr : ipNets) {
             if (match(cidr)) return true;
@@ -70,20 +93,11 @@ public class IPv4Matcher {
      * @param ipNets list of networks in CIDR format A.B.C.D/M
      * @return IP belongs to some of given network
      */
+    @Override
     public boolean matchAny(List<String> ipNets) {
         for (String cidr : ipNets) {
             if (match(cidr)) return true;
         }
         return false;
-    }
-
-    /**
-     * Validates input as IPv4 address.
-     *
-     * @param ip IPv4 with optional CIDR mask.
-     * @return given address is valid
-     */
-    public static boolean isValid(String ip) {
-        return ip.matches("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\/([0-9]|[1-2][0-9]|3[0-2]))?$");
     }
 }
